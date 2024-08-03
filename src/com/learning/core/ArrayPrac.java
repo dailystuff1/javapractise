cbd4e508a77986b6f57d314585f7ce66eceffe54
dev <dstech@gmail.com>
2024-08-03 19:52:28 +0530

    java practise

---
+++ src/com/learning/core/ArrayPrac.java
@@ -0,0 +1,12 @@
+package com.learning.core;
+
+public class ArrayPrac {
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		int id=10;
+		String name="abcd";
+		s1.setId(id);
+		s1.setName(name);
+	}
+
+}
